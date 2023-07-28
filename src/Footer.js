265f3dd55b366f009f0cadfdc0c97ccb48129b7d
aecamilleri <aecamilleri@gmail.com>
2023-07-28 14:55:59 -0300

    Cambios en Footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,9 @@ function Footer (){
     <div className="mx-auto max-w-screen-xl">
         <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-                <a href="https://flowbite.com" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
+            <a href="#" class="flex items-center">
+                <img src="./logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            </a>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                 <div>
@@ -48,7 +47,7 @@ function Footer (){
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" className="hover:underline">BlackMamba™</a>. All Rights Reserved.
             </span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,11 @@ import CartWidget from './CartWidget';
 function Header (){
     return (
         <header id="header" className="header flex justify-between py-4 px-2 mx-auto max-w-screen-xl  text-slate-800">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <div>
+            <a href="#" class="flex items-center">
+                <img src="./logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
             </a>
+            </div>
             <Navbar />
             <CartWidget itemCount={5} />
         </header>
